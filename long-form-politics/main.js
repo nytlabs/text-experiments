71ee064640a104f3292620ab0c7c837c82bb2fb9
Alexis Lloyd <alexislloyd@gmail.com>
2015-05-06 17:07:07 -0400

    merge

--- long-form-politics/main.js
+++ long-form-politics/main.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
 
 	//Handle a click on a link
 	$('a').on('click', function() {
-		console.log('click')
+		/*$('.context').slideUp();
+		$('a').removeClass('selected');*/
 		var parentEl = $(this).parent();
 		var thisIndex = $(parentEl).children('a').index(this);
 		var targetPara = $('#' + String($(parentEl).attr('id'))+'-context');
